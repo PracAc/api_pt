cd73ed41376f412922176bafd2ec51e6083ec5af
MinJae Kim <skwze@naver.com>
2024-10-11 15:26:43 +0900

    Initial commit

---
+++ src/main/java/org/example/api_pt/ApiPtApplication.java
@@ -0,0 +1,13 @@
+package org.example.api_pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/api_pt/ApiPtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.api_pt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
